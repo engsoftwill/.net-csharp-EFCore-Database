2feda7ba8f5cb8969d40c95bb5543af5a540e99f
engsoftwill <engeletwill@gmail.com>
2020-07-10 09:47:39 -0300

    rename variables and add comments

--- Source/Services/SubmissionsService.cs
+++ Source/Services/SubmissionsService.cs
@@ -6,19 +6,19 @@ namespace Codenation.Challenge.Services
 {
     public class SubmissionService : ISubmissionService
     {
-        public CodenationContext submissionR;
+        public CodenationContext codenationsubmission;
 
         public SubmissionService(CodenationContext context)
         {
-            submissionR = context;
+            codenationsubmission = context;
         }
 
         public IList<Submission> FindByChallengeIdAndAccelerationId(int challengeId, int accelerationId)
         {
-            return (submissionR.Candidates
+            return (codenationsubmission.Candidates
                         .Where(can => can.AccelerationId == accelerationId)
-                        .Join(submissionR.Users, can => can.UserId, usu => usu.Id, (can, usu) => usu)
-                        .Join(submissionR.Submissions, usu => usu.Id, sub => sub.UserId, (usu, sub) => sub)
+                        .Join(codenationsubmission.Users, can => can.UserId, usu => usu.Id, (can, usu) => usu)
+                        .Join(codenationsubmission.Submissions, usu => usu.Id, sub => sub.UserId, (usu, sub) => sub)
                         .Where(sub => sub.ChallengeId == challengeId)
                         .Distinct()).ToList();
 
@@ -27,7 +27,7 @@ namespace Codenation.Challenge.Services
 
         public decimal FindHigherScoreByChallengeId(int challengeId)
         {
-            var maiorScore = submissionR.Submissions
+            var maiorScore = codenationsubmission.Submissions
                 .OrderByDescending(x => x.Score)
                 .Where(x => x.ChallengeId == challengeId)
                 .Select(x => x.Score).First();
@@ -38,19 +38,19 @@ namespace Codenation.Challenge.Services
 
         public Submission Save(Submission submission)
         {
-            var temSubmissao = submissionR.Submissions.Find(submission.UserId, submission.ChallengeId);
+            var temSubmissao = codenationsubmission.Submissions.Find(submission.UserId, submission.ChallengeId);
 
             if (temSubmissao is null)
             {
-                submissionR.Submissions.Add(submission);
+                codenationsubmission.Submissions.Add(submission);
 
             }
             else
             {
-                submissionR.Submissions.Update(temSubmissao);
+                codenationsubmission.Submissions.Update(temSubmissao);
             }
 
-            submissionR.SaveChanges();
+            codenationsubmission.SaveChanges();
 
             return submission;
         }
